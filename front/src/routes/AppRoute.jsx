4f88b4be237ca747628401730e95c1280ebcf96a
cloudcoke <lms4296@gmail.com>
2023-03-29 14:59:26 +0900

    feat: actions test

--- front/src/routes/AppRoute.jsx
+++ front/src/routes/AppRoute.jsx
@@ -3,15 +3,15 @@ import { SignIn, SignUp, Main } from "../pages"
 import { BodyRoute } from "./BodyRoute"
 
 export const AppRouter = ({ List }) => {
-    console.log(List)
-    return (
-        <>
-            <Routes>
-                <Route path="/" element={<Main />}></Route>
-                <Route path="/signin" element={<SignIn />}></Route>
-                <Route path="/signup" element={<SignUp />}></Route>
-                <Route path="/quiz/*" element={<BodyRoute list={List} />} />
-            </Routes>
-        </>
-    )
+  console.log(List)
+  return (
+    <>
+      <Routes>
+        <Route path="/signin" element={<SignIn />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/quiz/*" element={<BodyRoute list={List} />} />
+        <Route path="*" element={<Main />}></Route>
+      </Routes>
+    </>
+  )
 }
