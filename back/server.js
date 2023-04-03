4d84e3134f7aa63dd4dc49e401cfa3411dc247e5
cloudcoke <lms4296@gmail.com>
2023-04-03 09:10:50 +0900

    feat: socket 수정

--- back/server.js
+++ back/server.js
@@ -1,44 +1,45 @@
-const express = require("express")
 const { createServer } = require("http")
 const app = require("./app")
 const { Server } = require("socket.io")
 const pty = require("node-pty")
 const os = require("os")
-const { readFileSync } = require("fs")
 
 const httpServer = createServer(app)
 const { localPort } = require("./config")
 const shell = os.platform() === "win32" ? "powershell.exe" : "bash"
 
-const ptyProcess = pty.spawn(shell, [], {
-    name: "xterm-color",
-    //   cwd: process.env.HOME,
-    env: process.env,
-})
-
+const terminals = new Map()
 const io = new Server(httpServer, {
-    cors: {
-        origin: true,
-        credentials: true,
-    },
+  cors: {
+    origin: true,
+    credentials: true,
+  },
 })
-io.on("connection", (socket) => {
-    console.log("new session")
-    socket.on("send", (data) => {
-        a = data
-        ptyProcess.write(`${data}\r`)
-        // ptyProcess.resize(100, 40)
-    })
 
-    ptyProcess.on("data", (datas) => {
-        socket.emit("data", `${datas}`)
-    })
-    socket.on("disconnect", () => {
-        console.log("프로세스 종료", 111)
-        ptyProcess.removeAllListeners("data")
-    })
+io.on("connection", (socket) => {
+  console.log("new session")
+  const term = pty.spawn(shell, [], {
+    name: "xterm-color",
+  })
+
+  terminals.set(socket, term)
+  term.write(`bash /home/ubuntu/user.sh\r`)
+  term.write(`clear\r`)
+
+  term.on("data", (data) => {
+    socket.emit("data", data)
+  })
+  socket.on("send", (data) => {
+    const terms = terminals.get(socket)
+    terms.write(`${data}\r`)
+  })
+
+  socket.on("disconnect", () => {
+    console.log("프로세스 종료", 111)
+    term.kill()
+  })
 })
 
 httpServer.listen(localPort, () => {
-    console.log(`Back Start on ${localPort}`)
+  console.log(`Back Start on ${localPort}`)
 })
